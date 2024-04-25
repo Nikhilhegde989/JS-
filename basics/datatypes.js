cf20ac9d3d974e6d83957811ea35b80ef9216bd4
Nikhilhegde989 <140316335+Nikhilhegde989@users.noreply.github.com>
2024-04-25 09:44:23 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) alert box will be popped out. but it requires browser so now it will give error.
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint for  the greater numbers
+// string => ""
+// boolean => true/false
+// null => its a standalone value
+// undefined => datatype of type undefined 
+// symbol => is used for uniqueness of components it is mostly used in react
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
