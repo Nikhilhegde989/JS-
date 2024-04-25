d8187600b4043299ee1dabbf7b04dd5eb545beba
Nikhilhegde989 <140316335+Nikhilhegde989@users.noreply.github.com>
2024-04-25 11:23:59 +0000

    DataConversions

---
+++ basics/datatypeConversions.js
@@ -0,0 +1,33 @@
+let score = "11"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let undef_Num_Conv = undefined
+let val = Number(undef_Num_Conv)
+console.log(val)
+console.log(typeof val)
+
+// Datatypes => Number
+// "33" => 33
+// "33abc" => NaN   Not a Number
+// true => 1; false => 0
+// null => 0
+// undefined = > NaN
+
+let isLoggedIn = "N"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Nikhil" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
